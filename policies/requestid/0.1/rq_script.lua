10a7df4c86639a4918d443b3fc3f7eba633580dd
pichayut <pichayut@mbixtech.com>
2019-08-07 15:00:42 +0700

    fix forwarded header to delete

--- policies/requestid/0.1/rq_script.lua
+++ policies/requestid/0.1/rq_script.lua
@@ -68,7 +68,7 @@ function _M:header_filter()
             xh = string.sub(k, 1, 2)
             cmh = string.sub(k, 1, 5)
             -- app_id, app_key and user_key cannot remove from response header, it can remove on request only
-            if xh == 'x-' or cmh == 'camel' or 'forwarded' then
+            if xh == 'x-' or cmh == 'camel' or k == 'forwarded' then
                 keep_h = '0'
                 if k == 'x-transaction-id' or k == 'x-correlation-id' or k == 'x-salt-hex' then
                     keep_h = '1'
