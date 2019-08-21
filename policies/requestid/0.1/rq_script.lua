927d01b0865db1e2795f637036b35ff81c16de51
pichayut <pichayut@mbixtech.com>
2019-08-21 12:00:10 +0700

    change log level from 0 (error) to info

--- policies/requestid/0.1/rq_script.lua
+++ policies/requestid/0.1/rq_script.lua
@@ -67,7 +67,7 @@ function _M:rewrite()
         ngx.req.clear_header('user_key')
     end
 
-    ngx.log(0, 'In coming request { ', header_val, ' : ', rq_uuid, ', { Body : ', ngx.var.request_body , ' } }')
+    ngx.log(ngx.INFO, 'In coming request { ', header_val, ' : ', rq_uuid, ', { Body : ', ngx.var.request_body , ' } }')
 
 end
 
@@ -123,7 +123,7 @@ function _M:body_filter()
         resp = ngx.ctx.buffered
     end
 
-    ngx.log(0, 'Out going response { ',header_val,' : ', rq_uuid, ', { Body : ', resp , ' } }')
+    ngx.log(ngx.INFO, 'Out going response { ',header_val,' : ', rq_uuid, ', { Body : ', resp , ' } }')
 
 end
 
